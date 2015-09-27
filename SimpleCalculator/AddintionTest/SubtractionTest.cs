80ca50397377dfa76d04dec5d81c8f9987078059
Chris_Jarvis <clj@ollure.com>
2015-09-27 00:40:10 -0500

    condensed test to single test project

---
+++ SimpleCalculator/AddintionTest/SubtractionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace SubtractionTest
+{
+    [TestClass]
+    public class SubtractionTest
+    {
+        [TestMethod]
+        public void subtractionTest()
+        {
+
+            Subtraction MyDifference = new Subtraction();
+            int answer = MyDifference.SubtractNum(5, 3);
+            Assert.AreEqual(answer, 2);
+        }
+    }
+}
